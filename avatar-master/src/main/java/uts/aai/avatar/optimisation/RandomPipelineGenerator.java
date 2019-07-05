3ddad96e084cb36e8873dd54772099b6271d2800
Tien Dung Nguyen <ntdzung.it@gmail.com>
2019-07-05 17:12:05 +1000

    random pipeline generator

--- avatar-master/src/main/java/uts/aai/avatar/optimisation/RandomPipelineGenerator.java
+++ avatar-master/src/main/java/uts/aai/avatar/optimisation/RandomPipelineGenerator.java
@@ -8,6 +8,9 @@ package uts.aai.avatar.optimisation;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 import uts.aai.mf.configuration.MLComponentConfiguration;
 import uts.aai.mf.model.MLComponent;
 import uts.aai.pn.utils.IOUtils;
@@ -22,15 +25,15 @@ public class RandomPipelineGenerator {
 
     public RandomPipelineGenerator(String datasetPath) {
         this.datasetPath = datasetPath;
-       
+
     }
 
-    public String generate() {
+    public String generateBPMNPipeline() {
         String componentStr = generateRandomComponents();
         System.out.println(componentStr);
         String componentTemplate = generateRandomComponents();
         ArrayList<MLComponent> orderedPipelineComponents = randomSelectAlgorithmsForPipeline(componentTemplate);
-        String bpmnPipeline = generateBPMNPipeline(orderedPipelineComponents);
+        String bpmnPipeline = RandomPipelineGenerator.this.generateBPMNPipeline(orderedPipelineComponents);
 
         return bpmnPipeline;
     }
@@ -70,14 +73,13 @@ public class RandomPipelineGenerator {
         List<MLComponent> filterList3 = listOfConfigurations.get(3);
         List<MLComponent> filterList4 = listOfConfigurations.get(4);
         List<MLComponent> classifierList = MLComponentConfiguration.initClassifier();
-        
+
         Collections.shuffle(filterList0);
         Collections.shuffle(filterList1);
         Collections.shuffle(filterList2);
         Collections.shuffle(filterList3);
         Collections.shuffle(filterList4);
         Collections.shuffle(classifierList);
-   
 
         ArrayList<MLComponent> orderedPipelineComponents = new ArrayList<>();
 
@@ -90,7 +92,6 @@ public class RandomPipelineGenerator {
 
         return orderedPipelineComponents;
 
-       
     }
 
     private String generateBPMNPipeline(ArrayList<MLComponent> orderedPipelineComponents) {
@@ -147,6 +148,165 @@ public class RandomPipelineGenerator {
         }
 
         return filterList;
+    }
+
+    public void generateBPMNTemplate() {
+        
+        String pipelineTemplate = "";
+        
+        ArrayList<String> listOfUUIDs = new ArrayList<>();
+
+        String scriptTaskBegin = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                + "<definitions xmlns=\"http://www.omg.org/spec/BPMN/20100524/MODEL\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:bpmndi=\"http://www.omg.org/spec/BPMN/20100524/DI\" xmlns:dc=\"http://www.omg.org/spec/DD/20100524/DC\" xmlns:di=\"http://www.omg.org/spec/DD/20100524/DI\" xmlns:g=\"http://www.jboss.org/drools/flow/gpd\" xmlns:tns=\"http://www.jboss.org/drools\" id=\"Definition\" targetNamespace=\"http://www.jboss.org/drools\" expressionLanguage=\"http://www.mvel.org/2.0\" typeLanguage=\"http://www.java.com/javaTypes\" exporter=\"Camunda Modeler\" exporterVersion=\"2.2.4\" xsi:schemaLocation=\"http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd\">\n"
+                + "  <process id=\"ml.process\" name=\"ML Process\" processType=\"Private\" isExecutable=\"true\">\n"
+                + "    <startEvent id=\"StartEvent\" name=\"StartEvent\">\n"
+                + "      <outgoing>#start_event_outgoing#</outgoing>\n"
+                + "    </startEvent>";
+
+        
+        String scriptTaskEnd = "<scriptTask id=\"#classifier#\" name=\"#classifier#\" scriptFormat=\"http://www.java.com/java\">\n"
+                + "      <incoming>#script_classifier_incoming#</incoming>\n"
+                + "      <outgoing>SequenceFlow_0tiw9jt</outgoing>\n"
+                + "      <script>kcontext.setVariable(\"#filter_input_data_name#\",\"#filter_input_data_value#\");\n"
+                + "                kcontext.setVariable(\"output-model-1\",\"C:/experiments/tools/avatar/output/model-1-output.model\");\n"
+                + "                \n"
+                + "                String predictorName = \"#classifier#\";\n"
+                + "          \n"
+                + "                uts.aai.pbmn.WekaExecutor wekaExecutor = new uts.aai.pbmn.WekaExecutor();\n"
+                + "                wekaExecutor.executePredictor(String.valueOf(kcontext.getVariable(\"#filter_input_data_name#\")),String.valueOf(kcontext.getVariable(\"output-model-1\")),predictorName);</script>\n"
+                + "    </scriptTask>\n"
+                + "    \n"
+                + "    \n"
+                + "    <endEvent id=\"EndEvent\" name=\"EndEvent\">\n"
+                + "      <incoming>SequenceFlow_0tiw9jt</incoming>\n"
+                + "      <terminateEventDefinition />\n"
+                + "    </endEvent>";
+        
+        
+        String filterScriptTaskTemplate = "<scriptTask id=\"#filter_#script_filter_no##\" name=\"#filter_#script_filter_no##\" scriptFormat=\"http://www.java.com/java\">\n"
+                + "      <incoming>#script_filter_incoming#</incoming>\n"
+                + "      <outgoing>#script_filter_outgoing#</outgoing>\n"
+                + "      <script>kcontext.setVariable(\"#filter_input_data_name#\",\"#filter_input_data_value#\");\n"
+                + "                kcontext.setVariable(\"#filter_output_data_name#\",\"#filter_output_data_value#\");\n"
+                + "                \n"
+                + "                String filterName = \"#filter_#script_filter_no##\";\n"
+                + "          \n"
+                + "                uts.aai.pbmn.WekaExecutor wekaExecutor = new uts.aai.pbmn.WekaExecutor();\n"
+                + "                wekaExecutor.executeFilter(String.valueOf(kcontext.getVariable(\"#filter_input_data_name#\")),String.valueOf(kcontext.getVariable(\"#filter_output_data_name#\")),filterName);</script>\n"
+                + "    </scriptTask>";
+        
+        
+        String sequenceFlowTemplate = "<sequenceFlow id=\"#sequence_flow_id#\" sourceRef=\"#sequence_flow_src#\" targetRef=\"#sequence_flow_target#\" />";
+        String str5 = "";
+
+        int numberOfPreprocessingComponents = randInt(1, 5);
+        
+        pipelineTemplate +=scriptTaskBegin;
+        
+   
+        for (int i=1;i<=numberOfPreprocessingComponents;i++) {
+            
+           String uuid = randUUID();
+           
+           if (i==1) {
+               pipelineTemplate.replaceAll("#start_event_outgoing#", uuid);
+               
+               String filterStr = new String(filterScriptTaskTemplate);
+               filterStr.replaceAll("#script_filter_incoming#", uuid);
+               filterStr.replaceAll("#script_filter_no#", String.valueOf(i));
+               
+               String filterInputName = "input-data-set-" + String.valueOf(i);
+               String filterInputValue = "#input_dataset#";
+               
+               String filterOutputName = "output-data-set-" + String.valueOf(i);
+               String filterOutputValue = "C:/experiments/tools/avatar/output/temp-data-out-"+String.valueOf(i)+".arff";
+               
+               
+               filterStr.replaceAll("#filter_input_data_name#", filterInputName);
+               filterStr.replaceAll("#filter_input_data_value#", filterInputValue);
+               filterStr.replaceAll("#filter_output_data_name#", filterOutputName);
+               filterStr.replaceAll("#filter_output_data_value#", filterOutputValue);
+               pipelineTemplate +=filterStr;
+               listOfUUIDs.add(uuid);
+               
+           } else if (i>1) {
+               
+              
+               pipelineTemplate.replaceAll("#script_filter_outgoing#", uuid);
+               
+               String filterStr = new String(filterScriptTaskTemplate);
+               filterStr.replaceAll("#script_filter_incoming#", uuid);
+               filterStr.replaceAll("#script_filter_no#", String.valueOf(i));
+               
+               String filterInputName = "input-data-set-" + String.valueOf(i);
+               String filterInputValue = "C:/experiments/tools/avatar/output/temp-data-out-"+String.valueOf(i-1)+".arff";
+               
+               String filterOutputName = "output-data-set-" + String.valueOf(i);
+               String filterOutputValue = "C:/experiments/tools/avatar/output/temp-data-out-"+String.valueOf(i)+".arff";
+               
+               
+               filterStr.replaceAll("#filter_input_data_name#", filterInputName);
+               filterStr.replaceAll("#filter_input_data_value#", filterInputValue);
+               filterStr.replaceAll("#filter_output_data_name#", filterOutputName);
+               filterStr.replaceAll("#filter_output_data_value#", filterOutputValue);
+               
+               
+               pipelineTemplate +=filterStr;
+               listOfUUIDs.add(uuid);
+           }
+           
+           
+           if (i==numberOfPreprocessingComponents) {
+               
+               
+                String uuidClassifier = randUUID();
+                pipelineTemplate.replaceAll("#script_filter_outgoing#", uuidClassifier);
+                
+                String classifierStr = new String(scriptTaskEnd);
+                classifierStr.replaceAll("#script_classifier_incoming#", uuidClassifier);
+                
+               
+                
+               String filterInputName = "input-data-set-" + String.valueOf(i+1);
+               String filterInputValue = "C:/experiments/tools/avatar/output/temp-data-out-"+String.valueOf(i)+".arff";
+               
+               classifierStr.replaceAll("#filter_input_data_name#", filterInputName);
+               classifierStr.replaceAll("#filter_input_data_value#", filterInputValue);
+               listOfUUIDs.add(uuidClassifier);
+               pipelineTemplate += classifierStr;
+           }
+           
+           
+        }
+    
+
+        listOfUUIDs.add("SequenceFlow_0tiw9jt");
+        
+        for (int i=0;i<listOfUUIDs.size();i++) {
+            String sequenceFlowId = listOfUUIDs.get(i);
+            String sequenceFlowSource = "";
+            String sequenceFlowTarget = "";
+            
+            if (i==0) {
+                sequenceFlowId 
+            }
+            
+            
+        }
+        
+        
+        
+    }
+
+    private int randInt(int min, int max) {
+
+        int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
+        return randomNum;
+    }
+    
+    private String randUUID(){
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString();
     }
 
 }
--- avatar-master/src/main/java/uts/aai/avatar/optimisation/RandomSearch.java
+++ avatar-master/src/main/java/uts/aai/avatar/optimisation/RandomSearch.java
@@ -19,14 +19,21 @@ import org.drools.KnowledgeBase;
 import org.drools.builder.KnowledgeBuilder;
 import org.drools.builder.KnowledgeBuilderFactory;
 import org.drools.builder.ResourceType;
+import org.drools.definition.process.Connection;
+import org.drools.definition.process.Node;
 import org.drools.io.Resource;
 import org.drools.io.ResourceFactory;
 import org.drools.io.impl.ByteArrayResource;
 import org.drools.runtime.StatefulKnowledgeSession;
+import org.jbpm.ruleflow.core.RuleFlowProcess;
+import org.jbpm.workflow.core.node.ActionNode;
+import org.jbpm.workflow.core.node.EndNode;
+import org.jbpm.workflow.core.node.StartNode;
 import uts.aai.avatar.model.AllEvaluationResult;
 import uts.aai.avatar.model.EvaluationResult;
 import uts.aai.global.AppConst;
 import uts.aai.mf.configuration.MLComponentConfiguration;
+import uts.aai.mf.model.MLComponent;
 import uts.aai.mf.model.MLComponentIO;
 import uts.aai.mf.service.DatasetMetaFeatures;
 import uts.aai.pbmn.SurrogatePipelineMapping;
@@ -49,7 +56,7 @@ public class RandomSearch {
 
     private String datasetPath;
     private long timeBudgetInMinutes;
-     private boolean isAvatar;
+    private boolean isAvatar;
     private ArrayList<EvaluationResult> evaluationResultList;
 
     public RandomSearch(String datasetPath, long timeBudgetInMinutes, boolean isAvatar) {
@@ -66,18 +73,11 @@ public class RandomSearch {
     public void start() {
 
         initRandomSearch();
-        
-        
-      
-        
 
         long startExperimentTime = System.currentTimeMillis();
 
-        while (!isTimeOut(startExperimentTime)) 
-       
-       //boolean isDiff = false;
-       
-       //while(!isDiff)
+        while (!isTimeOut(startExperimentTime)) //boolean isDiff = false;
+        //while(!isDiff)
         {
 
             cleanTemp();
@@ -85,26 +85,22 @@ public class RandomSearch {
             String bpmnPipeline = createRandomPipeline();
             long startEvaluationTime = System.currentTimeMillis();
             EvaluationResult evaluationResult = null;
-            String rs1="";
-            String rs2="";
-        
-            
-            if (isAvatar) 
-            {
+            String rs1 = "";
+            String rs2 = "";
+
+            if (isAvatar) {
                 System.out.println("START AVATAR");
-                
+
                 evaluationResult = evaluateAvatar(bpmnPipeline);
 
                 if (evaluationResult == null) {
 
                     evaluationResult = new EvaluationResult(bpmnPipeline, false, null);
                 }
-               rs2 = String.valueOf(evaluationResult.isValidity());
-               cleanTemp();
-               System.out.println("END AVATAR");
-            } 
-            else 
-            {
+                rs2 = String.valueOf(evaluationResult.isValidity());
+                cleanTemp();
+                System.out.println("END AVATAR");
+            } else {
                 System.out.println("START BPMN");
                 evaluationResult = evaluateBPMNPipeline(bpmnPipeline);
                 if (evaluationResult == null) {
@@ -113,11 +109,11 @@ public class RandomSearch {
                 }
                 rs1 = String.valueOf(evaluationResult.isValidity());
                 cleanTemp();
-                
+
                 System.out.println("END BPMN");
             }
-            
-            System.out.println(rs1+"-"+rs2);
+
+            System.out.println(rs1 + "-" + rs2);
 //
 //            
 //            if (!rs1.equals(rs2)) {
@@ -132,34 +128,32 @@ public class RandomSearch {
         }
 
     }
-    
+
     private String getDataPathFromScript(String sourceArff) {
-        
-      
-      
+
         String targetCSV = sourceArff.replace(".arff", ".csv");
-        
+
         fromArffToCSV(sourceArff, targetCSV);
-        
+
         return targetCSV;
     }
-    
-    private void fromArffToCSV(String sourceArff, String outputCSV){
-       
+
+    private void fromArffToCSV(String sourceArff, String outputCSV) {
+
         ArffLoader loader = new ArffLoader();
         try {
             loader.setSource(new File(sourceArff));
             Instances data = loader.getDataSet();
-            
+
             CSVSaver saver = new CSVSaver();
             saver.setInstances(data);
-            
+
             saver.setFile(new File(outputCSV));
             saver.writeBatch();
         } catch (IOException ex) {
             Logger.getLogger(SurrogatePipelineMapping.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
 
     public void finalise() {
@@ -219,7 +213,7 @@ public class RandomSearch {
 
     private String createRandomPipeline() {
         RandomPipelineGenerator randomPipelineGenerator = new RandomPipelineGenerator(datasetPath);
-        String bpmnPipeline = randomPipelineGenerator.generate();
+        String bpmnPipeline = randomPipelineGenerator.generateBPMNPipeline();
 
         return bpmnPipeline;
     }
@@ -260,29 +254,24 @@ public class RandomSearch {
     private EvaluationResult evaluateAvatar(String bpmnPipeline) {
 
         try {
-            
-        
-        SurrogatePipelineMapping spm = new SurrogatePipelineMapping();
-        PetriNetsPipeline petriNetsPipeline = spm.mappingFromBPMN2PetriNetsPipelineFromBPMNString(bpmnPipeline);
-        //System.out.println("\n" + petriNetsPipeline.toString());
 
-        
-        PetriNetsExecutionEngine engine = new PetriNetsExecutionEngine(petriNetsPipeline);
-        boolean result = engine.execute();
-        
-        
-        if (result) {
-            EvaluationResult evaluationResult = evaluateBPMNPipeline(bpmnPipeline);
-            return evaluationResult;
-        }
-        
+            SurrogatePipelineMapping spm = new SurrogatePipelineMapping();
+            PetriNetsPipeline petriNetsPipeline = spm.mappingFromBPMN2PetriNetsPipelineFromBPMNString(bpmnPipeline);
+            //System.out.println("\n" + petriNetsPipeline.toString());
+
+            PetriNetsExecutionEngine engine = new PetriNetsExecutionEngine(petriNetsPipeline);
+            boolean result = engine.execute();
+
+            if (result) {
+                EvaluationResult evaluationResult = evaluateBPMNPipeline(bpmnPipeline);
+                return evaluationResult;
+            }
+
         } catch (Exception e) {
         }
-        
+
         return null;
     }
-    
-   
 
     private void cleanTemp() {
         IOUtils iou = new IOUtils();
@@ -293,27 +282,116 @@ public class RandomSearch {
             Logger.getLogger(AutoProcessTestApp.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     private Token createToken(String filePath) {
-        
-    
-        
+
         List<Parameter> parameterList = new ArrayList<>();
         DatasetMetaFeatures datasetMetaFeatures = new DatasetMetaFeatures(filePath);
-        
+
         List<MLComponentIO> listOfMetaFeatures = datasetMetaFeatures.analyseMetaFeatures();
         System.out.println("");
         for (MLComponentIO mLComponentIO : listOfMetaFeatures) {
             System.out.println(mLComponentIO.getmLComponentCapability() + " : " + mLComponentIO.getValue());
             Parameter parameter = new Parameter(mLComponentIO.getmLComponentCapability().name(), mLComponentIO.getValue());
             parameterList.add(parameter);
-            
+
         }
-        
+
         Token token = new Token("metatoken", parameterList);
-        
-        
+
         return token;
+    }
+
+    public void mappingFromBPMN2NativeWekaCommand(String bpmnPipeline) {
+
+        ArrayList<MLComponent> listOfMLComponents = new ArrayList<>();
+        
+        
+        Resource resource = new ByteArrayResource(bpmnPipeline.getBytes());
+
+        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        kbuilder.add(resource, ResourceType.BPMN2);
+
+        KnowledgeBase kbase = kbuilder.newKnowledgeBase();
+
+        RuleFlowProcess process = (RuleFlowProcess) kbase.getProcess("ml.process");
+
+        StartNode startNode = process.getStart();
+        System.out.println("start node " + startNode.getName());
+
+        ActionNode currentActionNode = null;
+
+        boolean isReachEndEvent = false;
+
+        while (!isReachEndEvent) {
+
+            List<Connection> connectionList = null;
+            // start node
+
+            if (currentActionNode == null) {
+                connectionList = startNode.getDefaultOutgoingConnections();
+            } else {
+                connectionList = currentActionNode.getDefaultOutgoingConnections();
+            }
+
+            if (connectionList == null) {
+                isReachEndEvent = true;
+            } else {
+
+                for (Connection outConnection : connectionList) {
+                    Node node = outConnection.getTo();
+                    System.out.println("connection: " + outConnection.getToType());
+                    if (node instanceof EndNode) {
+                        isReachEndEvent = true;
+                        System.out.println("endnote: " + node.getName());
+//                        Place endPlace = createPlace(((EndNode) node).getUniqueId());
+//                        placeList.add(endPlace);
+                    } else {
+
+                        currentActionNode = (ActionNode) node;
+
+                        MLComponent mLComponent = MLComponentConfiguration.getComponentByID(currentActionNode.getName());
+                        listOfMLComponents.add(mLComponent);
+                        System.out.println("actionNode: " + currentActionNode.getName());
+                    }
+                }
+
+            }
+
+        }
+
+        generateFilteredClassiferWeka(listOfMLComponents);
+
+    }
+
+    private String prepareFullCommand(String outputFile, String pipeline) {
+        String command = "java -classpath C:/DATA/Projects/eclipse-workspace/aai_aw/weka-3-7-7/weka.jar weka.classifiers.meta.FilteredClassifier -t "
+                //+ "C:/DATA/Projects/eclipse-workspace/aai_aw/weka-3-7-7/data/iris.arff"
+                + datasetPath
+                + " "
+                + "-d " + AppConst.TEMP_EVALUATION_RESULT_PATH + outputFile + ".model "
+                + "-F \"weka.filters.MultiFilter "
+                + pipeline;
+
+//                + preprocessingConfigurations
+//                + "\" "
+//                + classifierConfiguration;
+        return command;
+    }
+
+    private String generateFilteredClassiferWeka(ArrayList<MLComponent> orderedPipelineComponents) {
+        String pipeline = orderedPipelineComponents.get(0).getComponentExecutionScriptFilteredClassifierWeka()
+                + orderedPipelineComponents.get(1).getComponentExecutionScriptFilteredClassifierWeka()
+                + orderedPipelineComponents.get(2).getComponentExecutionScriptFilteredClassifierWeka()
+                + orderedPipelineComponents.get(3).getComponentExecutionScriptFilteredClassifierWeka()
+                + orderedPipelineComponents.get(4).getComponentExecutionScriptFilteredClassifierWeka()
+                + "\" "
+                + orderedPipelineComponents.get(5).getComponentExecutionScriptFilteredClassifierWeka();
+
+        String outputFileName = "output_model";
+        String fullCommand = prepareFullCommand(outputFileName, pipeline);
+       
+        return fullCommand;
     }
 
 }
