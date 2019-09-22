6cf7d68a1a346206c9df7206e9f3e6ee9c3f423a
Tien Dung Nguyen <ntdzung.it@gmail.com>
2019-09-22 19:11:14 +1000

    get evaluation time of invalid pipelines - autosklearn

--- avatar-master/src/main/java/uts/aai/avatar/resultanalysis/AutoSklearnHistoryRunAnalyse.java
+++ avatar-master/src/main/java/uts/aai/avatar/resultanalysis/AutoSklearnHistoryRunAnalyse.java
@@ -28,23 +28,42 @@ public class AutoSklearnHistoryRunAnalyse {
         int memout = 0;
         int timeout = 0;
         int others = 0;
+        
+        double sucesss_time = 0.0;
+        double crashed_time = 0.0;
+        double memout_time = 0.0;
+        double timeout_time = 0.0;
+        double others_time = 0.0;
        
         JSONArray objData = objRoot.getJSONArray("data");
         for (int i=0; i<objData.length();i++) {
             
             JSONArray objElement =  objData.getJSONArray(i);
+            
+   
+            
             String pipelineStatus = objElement.getJSONArray(1).get(2).toString();
   
             if (pipelineStatus.contains("SUCCESS")) {
                 success++;
+                double tmp_time = Double.parseDouble(objElement.getJSONArray(1).get(1).toString());
+                sucesss_time += tmp_time;
             } else if (pipelineStatus.contains("CRASHED")) {
                 crashed++;
+                double tmp_time = Double.parseDouble(objElement.getJSONArray(1).get(1).toString());
+                crashed_time += tmp_time;
             } else if (pipelineStatus.contains("MEMOUT")) {
                 memout++;
+                double tmp_time = Double.parseDouble(objElement.getJSONArray(1).get(1).toString());
+                memout_time += tmp_time;
             } else if (pipelineStatus.contains("TIMEOUT")) {
                 timeout++;
+                double tmp_time = Double.parseDouble(objElement.getJSONArray(1).get(1).toString());
+                timeout_time += tmp_time;
             } else {
                 others++;
+                double tmp_time = Double.parseDouble(objElement.getJSONArray(1).get(1).toString());
+                others_time += tmp_time;
                 System.out.println("pipelineStatus: " + pipelineStatus);
             }
             
@@ -52,13 +71,16 @@ public class AutoSklearnHistoryRunAnalyse {
             
         }
         
-        System.out.println("Success: " + success);
-        System.out.println("crashed: " + crashed);
-        System.out.println("memout: " + memout);
-        System.out.println("timeout: " + timeout);
-        System.out.println("others: " + others);
+        System.out.println("Success: " + success + " - " + sucesss_time);
+        System.out.println("crashed: " + crashed + " - " + crashed_time);
+        System.out.println("memout: " + memout + " - " + memout_time);
+        System.out.println("timeout: " + timeout + " - " + timeout_time);
+        System.out.println("others: " + others + " - " + others_time);
         
-        System.out.println("RS: " + crashed + "/" + (success+crashed+memout+timeout+others));
+        System.out.println("Invalid pipelines/all: " + crashed + "/" + (success+crashed+memout+timeout+others));
+        System.out.println("Wasted Time: " + crashed_time + "/" + (sucesss_time+crashed_time+memout_time+timeout_time+others_time)+
+        " ~ " + (crashed_time/(sucesss_time+crashed_time+memout_time+timeout_time+others_time)));
+        
     }
     
     
---
+++ avatar-master/src/main/java/uts/aai/avatar/resultanalysis/AutoWekaWastedTime.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.aai.avatar.resultanalysis;
+
+/**
+ *
+ * @author ntdun
+ */
+public class AutoWekaWastedTime {
+    
+    //    =SUMIF(N:N,"CRASHED",H:H)/SUM(H:H)*100
+    
+}
